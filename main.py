5bf6cef55e6e409b2c1a1483f3a3d8cefb26e9cf
Vova1710 <115923775+Kit-mamu-mav@users.noreply.github.com>
2022-10-16 10:54:45 +0000

    say your name

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('a<50')
+class Human:
+  def __init__(self):
+    self.name = 'None'
+    self.age = 0
+    self.gender = 'None'
+  def introduce(self):
+    print('Hello, my name is', self.name)
+  def add_info(self):
+    self.name = input('Name: ')
+    self.age = int(input('Age: '))
+    self.gender = input('Gender: ')
+
+obj = Human()
+obj.introduce()
+obj.add_info()
+obj.introduce()
